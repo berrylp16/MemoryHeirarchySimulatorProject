f2b517fe641d35309e227727734cba41e2a6e09b
berrylp16 <113481157+berrylp16@users.noreply.github.com>
2024-03-28 11:17:43 -0400

    Update Program.cs
    added FIFO with more little changes

--- memoryProject/Program.cs
+++ memoryProject/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 
 
 class RWData
@@ -255,10 +256,68 @@ class RWData
     
 }
 
-    private static void firstInFirstOut(Data[] virtualMemory)
+ static void firstInFirstOut(Data[] virtualMemory)
     {
-        //todo
-        //we can print all output within the function, or change void to string and print as its called.
+        Console.WriteLine("FIFO Algorithm:\n");
+        Console.WriteLine("Virtual Address\tVirtual Pg #\tPage Off\tTable Result\tPhysical Page #");
+        Console.WriteLine("---------------|-------------|----------|----------------------|-------------------|");
+        int cacheSize = 20;
+        Data[] cache = new Data[cacheSize];
+        int physicalPageNumber = -1;
+        int totalHit = 0;
+        int totalMiss = 0;
+
+        for (int index = 0; index < virtualMemory.Length; index++)
+        {
+            Data currentData = virtualMemory[index];
+            string stringData = currentData.ToString();
+            var virt = Regex.Match(stringData, @"(.{9})\s*$");
+            string virtualAddressP = virt.ToString();
+            string virtualAddress = virtualAddressP.Replace(")", "");
+            char virtualPage = stringData[^4];
+            var pageOffsetPV = Regex.Match(stringData, @"(.{3})\s*$");
+            string pageOffsetP = pageOffsetPV.ToString();
+            string pageOffset = pageOffsetP.Replace(")", "");
+            bool inCache = false;
+            bool tableResult = false;
+
+            for (int i = 0; i < cache.Length; i++)
+            {
+                if (cache[i]._hex == currentData._hex)
+                {
+                    inCache = true;
+                    tableResult = true;
+                    physicalPageNumber = i;
+                    break;
+                }
+            }
+
+            if (inCache)
+            {
+                totalHit++;
+            }
+            else
+            {
+                totalMiss++;
+
+                for (int i = 0; i < cache.Length - 1; i++)
+                {
+                    cache[i] = cache[i + 1];
+                }
+                cache[cache.Length - 1] = currentData;
+                cache[cache.Length - 1].physicalPageNumber = physicalPageNumber;
+                physicalPageNumber = (physicalPageNumber + 1) % cache.Length;
+            }
+
+            string tableResultString = tableResult ? "hit" : "miss";
+            Console.WriteLine($"{virtualAddress}\t\t{virtualPage}\t    {pageOffset}\t\t\t{tableResultString}\t\t\t{physicalPageNumber}");
+        }
+
+        Console.WriteLine("Simulation Statistics");
+        Console.WriteLine($"Total Hit: {totalHit}");
+        Console.WriteLine($"Total Miss: {totalMiss}");
+        double hitRatio = totalMiss != 0 ? (double)totalHit / totalMiss : 0;
+        Console.WriteLine($"Hit Ratio: {hitRatio}");
     }
 
 }
